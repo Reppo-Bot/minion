8b287074f3b7c24689e5c0d89daafd460b2dff93
Josh <josh@mjbrowns.com>
2022-03-17 10:38:43 -0400

    feat: add api test with leaderboard

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 import SlashDiscord
 from dotenv import dotenv_values
 import sys
+import requests as r
 
-
-def ping(ctx):
-    return f"Hey {ctx.member.user.username}"
+def leaderboard(ctx):
+    res = r.post("http://bot.localhost:8080/command/callCommand", json=ctx)
+    if(res.json()["message"] != ""):
+        return {'error' : True, "message" : res.json()['message']}
+    return {'error' : False}
 
 if(__name__ == "__main__"):
     config = dotenv_values(".env")
-    _ping = SlashDiscord.Command("ping", 1, "ping", handler=ping)
-    _client = SlashDiscord.Client(config["TOKEN"], 0, config["APP_ID"], log_level=10, commands=[_ping])
+    _command = SlashDiscord.Command("leaderboard", 1, "just testing", handler=leaderboard, respond=False)
+    _client = SlashDiscord.Client(config["TOKEN"], 0, config["APP_ID"], log_level=10, commands=[_command])
     if("-d" in sys.argv):
         _client.deregister()
         _client.register()
