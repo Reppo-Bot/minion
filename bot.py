7b11ee16006c8b434cfe86ed50f2cb66541a8214
Josh <josh@mjbrowns.com>
2022-03-22 16:14:26 -0400

    feat: add api forwarding

--- bot.py
+++ bot.py
@@ -3,16 +3,22 @@ from dotenv import dotenv_values
 import sys
 import requests as r
 
-def leaderboard(ctx):
+def default(ctx):
     res = r.post("http://bot.localhost:8080/command/callCommand", json=ctx)
-    if(res.json()["message"] != ""):
-        return {'error' : True, "message" : res.json()['message']}
-    return {'error' : False}
+    _res = res.json()
+    if("failed" in _res):
+        print(f"Failed command, responding with {_res['failed']}")
+        return _res["failed"]
+
+def ping(ctx):
+    return f"Hello {ctx.member.nick}!"
 
 if(__name__ == "__main__"):
     config = dotenv_values(".env")
-    _command = SlashDiscord.Command("leaderboard", 1, "just testing", handler=leaderboard, respond=False)
-    _client = SlashDiscord.Client(config["TOKEN"], 0, config["APP_ID"], log_level=10, commands=[_command])
+    _default = SlashDiscord.Command("default", 1, "default command", handler=default, respond=False)
+    _ping = SlashDiscord.Command("ping", 1, "say hello", handler=ping, respond=True)
+    _client = SlashDiscord.Client(config["TOKEN"], 0, config["APP_ID"], log_level=10, commands=[_ping])
+    _client.setDefault(_default)
     if("-d" in sys.argv):
         _client.deregister()
         _client.register()
